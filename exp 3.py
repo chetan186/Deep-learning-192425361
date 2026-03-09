9d503583679ea44691c845778c846b9f8f9c31de
chetan186 <yarragorlachetan@gmail.com>
2026-03-09 11:05:53 +0530

    Add files via upload

---
+++ exp 3.py
@@ -0,0 +1,50 @@
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import (
+    confusion_matrix,
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score
+)
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+X, y = load_digits(return_X_y=True)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25, random_state=23
+)
+
+clf = RandomForestClassifier(random_state=23)
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+cm = confusion_matrix(y_test, y_pred)
+
+plt.figure(figsize=(8,6))
+sns.heatmap(
+    cm,
+    annot=True,
+    fmt='g',
+    cmap="winter"
+)
+
+plt.ylabel('Prediction', fontsize=13)
+plt.xlabel('Actual', fontsize=13)
+plt.title('Confusion Matrix', fontsize=17)
+plt.show()
+
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+
+precision = precision_score(y_test, y_pred, average='macro')
+recall = recall_score(y_test, y_pred, average='macro')
+f1 = f1_score(y_test, y_pred, average='macro')
+
+print("Precision:", precision)
+print("Recall:", recall)
+print("F1 Score:", f1)
